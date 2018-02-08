5c96acfcfd94f64590138175a8de375ba98ca5aa
ashleijo <shuryjo@gmail.com>
2018-02-08 16:17:43 -0500

    fixing files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './styles/App.css';
 import LeftPanel from './components/LeftPanel/LeftPanel';
 import PendingConnections from './components/PendingConnections/PendingConnections';
